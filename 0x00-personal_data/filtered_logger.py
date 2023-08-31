0e093bbc54461ac932d753ff103c3a10f7f8560d
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-31 10:07:51 +0300

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python3
 """
-Module to obfuscate log fields using regular expressions.
+Module for database connection.
 """
 
-
-import logging
-import re
-from typing import List
+import os
+import mysql.connector
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+def get_db():
+    """
+    Create a connection to the MySQL db using env variables for credentials
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    Returns:
+        mysql.connector.connection.MySQLConnection: The db connection object.
+    """
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
 
-    def __init__(self, fields: List[str]):
-        """
-        The itself funtion defination
-        """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+    db = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
 
-    def format(self, record: logging.LogRecord) -> str:
-        """
-        The format logging record function
-        """
-        log_message = super().format(record)
-        for field in self.fields:
-            log_message = re.sub(rf'{field}=([^;]+)',
-                                 f'{field}={self.REDACTION}', log_message)
-        return log_message
+    return db
