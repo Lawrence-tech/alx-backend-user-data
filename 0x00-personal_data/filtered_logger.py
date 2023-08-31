50827ba2199f6e6b1902d2054d3b0ac2f9b7e041
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-31 09:50:11 +0300

    Getting to solve requirement error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,10 +16,16 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        The itself function defination field
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Format logged record within the function
+        """
         log_message = super().format(record)
         for field in self.fields:
             log_message = log_message.replace(field + '=', field + '=' +
