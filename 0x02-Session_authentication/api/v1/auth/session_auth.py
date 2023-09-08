8decf3ebac53edd89eb589c2fd64ebb14655f243
Lawrence-tech <lawrencesagini2@gmail.com>
2023-09-08 05:06:54 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+A module class SessionAuth that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    def __init__(self):
+        # Initialize any necessary data for session-based authentication
+        pass
+
+    def check_session_token(self, request):
+        # Implement logic to check and validate session tokens
+        pass
+
+    def authenticate_user(self, request):
+        # Implement user authentication logic for sessions
+        pass
