24201181e94657bfd0febcba5fa3d9dae13299fc
Lawrence-tech <lawrencesagini2@gmail.com>
2023-09-08 04:13:06 +0300

    added documentation to app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,21 +25,24 @@ if auth_type == 'basic_auth':
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler.
+    """
+    The Not found function handler.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler.
+    """
+    Fxn for the Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler.
+    """
+    Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
 
